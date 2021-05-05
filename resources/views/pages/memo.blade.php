a04324f62ffd743fba01019efbe06c58d626d8b7
risarisarisa <from.aqua.to@gmail.com>
2021-05-06 00:49:56 +1000

    deleted test code

--- resources/views/pages/memo.blade.php
+++ resources/views/pages/memo.blade.php
@@ -8,5 +8,4 @@
     <li class="list-group-item">Laravel Mixのインストール</li>
     <li class="list-group-item">Laravel uiインストール(<a target="_blank" href="https://uedive.net/2019/195/la6fa/">Ref</a>)</li>
   </ul>
-  <p>hoge</p>
 @endsection
