55ab30b5cb750e6edeaf32910deb26c3dca263db
risarisarisa <from.aqua.to@gmail.com>
2021-05-07 19:26:09 +1000

    Added a button "create new" for articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,9 +1,13 @@
 @extends('layout')
 @section('content')
-
+<div class="header d-flex justify-content-between">
   <h1>Articles</h1>
-
+  <div class="">
+    <a href='./articles/create' class="btn btn-primary">Create New</a>
+  </div>
+</div>
   <hr/>
+
 
   @foreach($articles as $article)
     <article>
