f93d6bd89bfe5986a3101e34b3727e080b7762e4
risarisarisa <from.aqua.to@gmail.com>
2021-05-11 17:14:06 +1000

    Added delete button

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -54,4 +54,11 @@ class ArticlesController extends Controller
       return redirect(url('articles', [$article->id]));
     }
 
+    public function destroy($id) {
+      $article = Article::findOrFail($id);
+      $article->delete();
+
+      return redirect('articles');
+    }
+
 }
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -10,12 +10,16 @@
   </article>
 
   <hr/>
-  
+
   <div>
     <a href="{{ action('App\Http\Controllers\ArticlesController@edit', [$article->id]) }}"
       class="btn btn-primary">
       Edit
     </a>
+    {!! Form::open(['method' => 'DELETE', 'url' => ['articles', $article->id], 'class' => 'd-inline']) !!}
+      {!! Form::submit('DELETE', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+
     <a href="{{ action('App\Http\Controllers\ArticlesController@index') }}"
       class="btn btn-secondary float-right">
       Back to a list
