87ee377b5bf6b65e095939e3f8c41aa8855fc34d
risarisarisa <from.aqua.to@gmail.com>
2021-05-21 21:02:11 +1000

    Only login user can edit and delete articles

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -12,13 +12,25 @@
   <hr/>
 
   <div>
-    <a href="{{ route('articles.edit', [$article->id]) }}"
-      class="btn btn-primary">
-      Edit
-    </a>
-    {{-- {!! delete_form(['articles', $article->id]) !!} --}}
-    {!! delete_form(route('articles.destroy', [$article->id])) !!}
-
+    @if (Route::has('login'))
+      @auth
+        <a href="{{ route('articles.edit', [$article->id]) }}"
+          class="btn btn-primary">
+          Edit
+        </a>
+        {{-- {!! delete_form(['articles', $article->id]) !!} --}}
+        {!! delete_form(route('articles.destroy', [$article->id])) !!}
+      @else
+        <a href="{{ route('articles.edit', [$article->id]) }}"
+          class="btn btn-primary disabled" aria-disabled="true">
+          Edit
+        </a>
+        <a href="#"
+          class="btn btn-danger disabled" aria-disabled="true">
+          Delete
+        </a>
+      @endauth
+    @endif
     <a href="{{ route('articles.index') }}"
       class="btn btn-secondary float-right">
       Back to a list
