b66b68289c58d7863339ab221b3b60b5cc0d1f71
risarisarisa <from.aqua.to@gmail.com>
2021-07-03 12:23:26 +1000

    -practice
    updated Dashboard

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,6 +1,20 @@
 <div class="p-6">
-  <h2>DASHBOARD</h2>
-  <p>
-    This is a practie
-  </p>
+  <h2><img src="{{ Auth::user()->profile_photo_url }}">My details</h2>
+  {{-- <p>
+    {{ Auth::user() }}
+  </p> --}}
+  <div class="card">
+    <div class="card-header">
+
+    </div>
+    <div class="card-body">
+      <ul class="list-group">
+        <li class="list-group-item"> ID: {{ Auth::user()->id }} </li>
+        <li class="list-group-item"> Name: {{ Auth::user()->name }} </li>
+        <li class="list-group-item"> Email: {{ Auth::user()->email }} </li>
+        <li class="list-group-item"> Created Date: {{ Auth::user()->created_at }} </li>
+        {{-- <li class="list-group-item"> <a href="{{ Auth::user()->profile_photo_url }}">Portfolio</a> </li> --}}
+     </ul>
+   </div>
+ </div>
 </div>
